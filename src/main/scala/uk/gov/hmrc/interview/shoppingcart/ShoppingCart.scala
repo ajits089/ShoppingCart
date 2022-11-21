14736c61bad2c3f69c791e9148c04aeca293f899
Ajit Kumar <ajitk23@apple.com>
2022-11-21 19:37:53 +0530

    step-2 Shopping Cart Application With Discounts

--- src/main/scala/uk/gov/hmrc/interview/shoppingcart/ShoppingCart.scala
+++ src/main/scala/uk/gov/hmrc/interview/shoppingcart/ShoppingCart.scala
@@ -1,27 +1,51 @@
 package uk.gov.hmrc.interview.shoppingcart
 
+import uk.gov.hmrc.interview.shoppingcart.Fruit.{Apple, Orange}
+import uk.gov.hmrc.interview.shoppingcart.Offer.{AppleBuyOneGetOne, Orange3For2}
+
 case class Pence(value: Long)
 
 sealed trait Fruit {
-  val cost: Pence
+  val cost:Pence
 }
 
 object Fruit {
   case object Apple extends Fruit {
     override val cost: Pence = Pence(60)
   }
-
   case object Orange extends Fruit {
     override val cost: Pence = Pence(25)
   }
 }
 
-object ShoppingCart {
+sealed trait Offer
 
-  def checkout(cart: List[Fruit]): Pence = {
-    val total = cart.foldLeft[Long](0)((total, item) => total + item.cost.value)
-
-    Pence(total)
-  }
+object Offer {
+  case object AppleBuyOneGetOne extends Offer
+  case object Orange3For2 extends Offer
 }
 
+object ShoppingCart {
+
+  def checkout(cart: List[Fruit], offers: List[Offer] = List.empty): Pence = {
+    val total = cart.foldLeft[Long](0)((total, item) => total + item.cost.value)
+
+    val discount = offers.map {
+      case AppleBuyOneGetOne => appleDiscount(cart)
+      case Orange3For2 => orangeDiscount(cart)
+      case _ => 0
+    }.sum
+
+    Pence(total-discount)
+  }
+
+  private def appleDiscount(cart: List[Fruit]): Long = {
+    val numberOfDiscountedApples = cart.count(_ == Apple) / 2
+    Apple.cost.value * numberOfDiscountedApples
+  }
+
+  private def orangeDiscount(cart:List[Fruit]): Long = {
+    val numberOfDiscountedOranges = cart.count(_ == Orange) / 3
+    Orange.cost.value * numberOfDiscountedOranges
+  }
+}
--- src/test/scala/uk/gov/hmrc/interview/shoppingcart/ShoppingCartSpec.scala
+++ src/test/scala/uk/gov/hmrc/interview/shoppingcart/ShoppingCartSpec.scala
@@ -3,6 +3,7 @@ package uk.gov.hmrc.interview.shoppingcart
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import uk.gov.hmrc.interview.shoppingcart.Fruit.{Apple, Orange}
+import uk.gov.hmrc.interview.shoppingcart.Offer.{AppleBuyOneGetOne, Orange3For2}
 import uk.gov.hmrc.interview.shoppingcart.ShoppingCart.checkout
 
 class ShoppingCartSpec extends AnyFlatSpec {
@@ -31,5 +32,44 @@ class ShoppingCartSpec extends AnyFlatSpec {
     checkout(List(Apple, Orange)) shouldBe Pence(85)
     checkout(List(Apple, Orange, Apple, Orange)) shouldBe Pence(170)
     checkout(List(Apple, Apple, Orange, Apple)) shouldBe Pence(205)
+  }
+
+  // Step 2 tests with discounts
+
+  it should "return correct total when there are only apple discounts" in {
+    val offers = List(AppleBuyOneGetOne)
+    checkout(List.fill(1)(Apple), offers) shouldBe Pence(60)
+    checkout(List.fill(2)(Apple), offers) shouldBe Pence(60)
+    checkout(List.fill(3)(Apple), offers) shouldBe Pence(120)
+    checkout(List.fill(4)(Apple), offers) shouldBe Pence(120)
+    checkout(List.fill(5)(Apple), offers) shouldBe Pence(180)
+    checkout(List.fill(10)(Apple), offers) shouldBe Pence(300)
+
+    checkout(List.fill(3)(Apple) ++ List(Orange), offers) shouldBe Pence(145)
+  }
+
+  it should "return correct total when there are only orange discounts" in {
+    val offers = List(Orange3For2)
+    checkout(List.fill(1)(Orange), offers) shouldBe Pence(25)
+    checkout(List.fill(2)(Orange), offers) shouldBe Pence(50)
+    checkout(List.fill(3)(Orange), offers) shouldBe Pence(50)
+    checkout(List.fill(4)(Orange), offers) shouldBe Pence(75)
+    checkout(List.fill(5)(Orange), offers) shouldBe Pence(100)
+    checkout(List.fill(7)(Orange), offers) shouldBe Pence(125)
+
+    checkout(List.fill(5)(Orange) ++ List(Apple), offers) shouldBe Pence(160)
+  }
+
+  it should "return correct total when there are discounts on both apples and oranges" in {
+    val offers = List(AppleBuyOneGetOne, Orange3For2)
+    checkout(List.fill(2)(Orange) ++ List.fill(2)(Apple), offers) shouldBe Pence(110)
+    checkout(List.fill(3)(Orange) ++ List.fill(3)(Apple), offers) shouldBe Pence(170)
+    checkout(List.fill(5)(Orange) ++ List.fill(3)(Apple), offers) shouldBe Pence(220)
+    checkout(List.fill(7)(Orange) ++ List.fill(7)(Apple), offers) shouldBe Pence(365)
+  }
+
+  it should "return 0 when the cart is empty but there are some discounts" in {
+    val offers = List(AppleBuyOneGetOne, Orange3For2)
+    checkout(List.empty, offers) shouldBe Pence(0)
   }
 }
